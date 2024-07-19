8ed020b27725bd06cf9b04ec63d59428de850c7d
emreagann <159646253+emreagann@users.noreply.github.com>
2024-07-19 19:10:26 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,57 @@
+
+const quiz = [
+    {
+        q: 'Which month comes right before June?',
+        options: ['May', 'July', 'September', 'August'],
+        answer: 0
+    },
+    {
+        q:'What color is a banana?',
+        options:['Red','Yellow','White','Blue'],
+        answer:1
+    },
+    {
+        q:'3+4=7?',
+        options:['true','false'],
+        answer:0
+    },
+    {
+        q: 'What time of day do we have breakfast?',
+        options: ['In the afternoon', 'In the evening', 'In the morning'],
+        answer:2
+    },
+    {
+        q: 'What is the result of 22 + 6?',
+        options: ['99', '56', '16', '28'],
+        answer:3
+    },
+    {
+        q: 'How many squares are there in the following figure?',
+        options: ['35', '30', '40', '50'],
+        answer: 2,
+        img:'file:///C:/Users/Monster/OneDrive/Masa%C3%BCst%C3%BC/square.jpg'
+    },
+    {
+        q: 'Count the triangles in picture below.',
+        options: ['7', '9', '12', '13'], 
+        answer: 3,
+        img: 'file:///C:/Users/Monster/OneDrive/Masa%C3%BCst%C3%BC/triangle.jpg'
+
+    },
+    {
+        q: 'Windows,macOS,and Linux are examples of ________________.?',
+        options: ['web browsers', 'mobile devices', 'filmy heroines', 'operating systems'],
+        answer:3,
+
+    },
+    {
+        q: 'What does "GUI" stand for?',
+        options: ['Global user index', 'Graphical user interface', 'golu use iphone'],
+        answer:1,
+    },
+    {
+        q: 'Mark Zuckerberg is the owner of ?',
+        options: ['facebook', 'google', 'linux', 'linkedin'],
+        answer: 0,
+    },
+]
